43fdabb8b69fb113fae575605b58d60897059622
Chris Plock <plockc@gmail.com>
2013-03-30 15:27:00 -0700

    fixing metwork and adding openssh and dropping the vi

--- archInstall/archInstall.sh
+++ archInstall/archInstall.sh
@@ -22,7 +22,7 @@ mount -t ext4 /dev/sda1 /mnt/boot
 mkswap /dev/sda3
 swapon /dev/sda3
 
-pacstrap /mnt base base-devel php-apc php-cgi php-fpm php-sqlite lighttpd dokuwiki augeas wget unzip
+pacstrap /mnt base base-devel openssh php-apc php-cgi php-fpm php-sqlite lighttpd dokuwiki augeas wget unzip
 
 
 genfstab -p /mnt >> /mnt/etc/fstab
@@ -32,14 +32,14 @@ mkinitcpio -p linux
 pacman --noconfirm -S syslinux
 
 sed -i 's/sda3/sda2/' /boot/syslinux/syslinux.cfg
-vi /boot/syslinux/syslinux.cfg
 syslinux-install_update -iam # install files(-i), set boot flag (-a), install MBR boot code (-m)
 
 echo "doku.plock.org" > /etc/hostname
 ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 locale-gen # edit /etc/locate.gen possibly
-systemctl enable dhcpcd@enp0s5
+#systemctl enable dhcpcd@enp0s5
+#systemctl enable dhcpcd@eth0.service
 exit # exit the chroot
 EOF
 

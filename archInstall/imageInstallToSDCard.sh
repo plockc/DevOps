993b3a34387a4aab9e264956a5d26407ba3777dc
Chris Plock <plockc@gmail.com>
2013-05-29 08:42:43 -0700

    making it easier to read

--- archInstall/imageInstallToSDCard.sh
+++ archInstall/imageInstallToSDCard.sh
@@ -21,7 +21,11 @@ if [[ $newDisk == "" ]]; then echo && echo No new disk found! && echo && exit; e
 newDiskEscaped=${newDisk//\//\\\/} # convert / to escaped slash: \/
 
 echo
-diskutil list | awk "/^\/dev\/disk/ {record=0} "/$newDiskEscaped/' {record=1} {if (record) diskDetail = diskDetail $0  "\n"} END {print diskDetail}'
+diskutil list
+  | awk "/^\/dev\/disk/ {record=0} \
+     "/$newDiskEscaped/' {record=1} \
+     {if (record) diskDetail = diskDetail $0  "\n"} \
+     END {print diskDetail}'
 echo
 read -p "Are you sure you want to destroy $newDisk with the contents of $1 ? [yN] "
 
@@ -32,6 +36,8 @@ then
 fi
 
 newDisk=${newDisk/disk/rdisk} # use the raw disk to avoid the buffering, much faster
+
 sudo diskutil unmountDisk $newDisk
-echo && echo Copying Image, this will take a while
+
+echo && echo Copying Image, this will take about a minute per GB on a class 10 card
 sudo dd bs=16m if="$1" of=$newDisk

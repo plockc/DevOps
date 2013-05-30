215c9362e1b1f7b059b74c0c0fc90af76f942442
Chris Plock <plockc@gmail.com>
2013-05-29 21:46:23 -0700

    asks for password and hostname and splits out dokuwiki

--- archInstall/archInstall.sh
+++ archInstall/archInstall.sh
@@ -37,7 +37,7 @@ mount -t ext4 /dev/sda1 /mnt/boot
 mkswap /dev/sda3
 swapon /dev/sda3
 
-pacstrap /mnt base base-devel openssh php-apc php-cgi php-sqlite lighttpd dokuwiki augeas ntp wget darkhttpd darkstat unzip dnsutils rsync screen
+pacstrap /mnt base base-devel openssh augeas ntp wget darkhttpd darkstat unzip dnsutils rsync screen
 
 genfstab -p /mnt >> /mnt/etc/fstab
 
@@ -48,18 +48,41 @@ pacman --noconfirm -S syslinux
 sed -i 's/sda3/sda2/' /boot/syslinux/syslinux.cfg
 syslinux-install_update -iam # install files(-i), set boot flag (-a), install MBR boot code (-m)
 
-echo "doku.plock.org" > /etc/hostname
 ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 locale-gen # edit /etc/locate.gen possibly
 #systemctl enable dhcpcd@enp0s5
 # install network device is eth0 but runtime is emp0s5, so have to do it manually
 ln -s '/usr/lib/systemd/system/dhcpcd\@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd\@enp0s5.service'
-systemctl enable sshd.service darkstat ntpd.service lighttpd.service
+systemctl enable sshd.service darkstat ntpd.service
 
-chpasswd -e << EOSF
-root:\$6\$BcIn6ZXm\$dsIT5df3t.iNCQUbYMTVMuublLUUC0s4RjUknQfIPYtvpGlivPH9Srq4Ho/Oh1n/PoLuNHiH/C7O4nb6JC55A.
+## CHANGE ROOT PASSWORD
+
+read -s -p "Please enter a new root password: "
+NEW_PASSWORD=$REPLY
+echo
+read -s -p "Please confirm: "
+echo
+
+if [[ ! $REPLY == $NEW_PASSWORD ]]
+then
+  echo Passwords did not match, please try again
+  exit;
+fi
+chpasswd << EOSF
+root:$NEW_PASSWORD
 EOSF
+
+## CHANGE HOSTNAME
+read -p "Please enter the full host name for this Pi: "
+HOSTNAME=$REPLY
+if [[ $HOSTNAME == "" ]]
+then
+echo Please try again with a valid host name
+exit;
+fi
+echo $HOSTNAME > /etc/hostname
+
 
 exit # exit the chroot
 EOF

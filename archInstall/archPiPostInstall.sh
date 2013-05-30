22afb168a7a81e4f57a7e8f0fa7a4eeb42777e72
Chris Plock <plockc@gmail.com>
2013-05-29 17:13:03 -0700

    cleaner install
    adding screen and noconfirm during pacman package installs
    
    checks if password is still the default and if so prompts for new
    password
    
    checks for default hostname and prompts for new one, resizes filesystem
    to full size of the partition

--- archInstall/archPiPostInstall.sh
+++ archInstall/archPiPostInstall.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-# USAGE: bash <(curl -fsSL https://raw.github.com/plockc/ArchDevOps/master/archInstall/archInstall.sh)
+# USAGE: bash <(curl -fsSL https://raw.github.com/plockc/ArchDevOps/master/archInstall/archPiPostInstall.sh)
 
 # TODO: request for hostname and for a initial password
 # TODO: options for a Arch package cache, or personal wiki/blog, or something else
 
 set -e
 
-pacman -Sy php-apc php-cgi php-sqlite lighttpd dokuwiki augeas darkstat unzip dnsutils rsync
+pacman --noconfirm -Sy --needed php-apc php-cgi php-sqlite lighttpd dokuwiki augeas darkstat unzip dnsutils rsync screen
 
-echo "doku.plock.org" > /etc/hostname
-ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime
+# CHANGE PASSWORD IF STILL THE DEFAULT "ROOT"
+salt=`grep root /etc/shadow | sed 's/root:\(\$.*\$.*\)\$.*/\1/'`
+defaultPass=`php -r "echo crypt('root', \"${salt//\$/\\\\\\$}\") . '\n';"`
+if grep -q $defaultPass /etc/shadow;
+  read -s -p "Please enter a new root password: "
+  NEW_PASSWORD=$REPLY
+  echo
+  read -s -p "Please confirm: "
+  echo
+
+  if [[ ! $REPLY == $NEW_PASSWORD ]]
+  then
+      echo Passwords did not match, please try again
+      exit;
+  fi
+  chpasswd << EOSF
+  root:$NEW_PASSWORD
+  EOSF
+fi
+
+# UPDATE HOSTNAME
+if grep alarmpi <<< `hostname`; then
+  read -p "Please enter the full host name for this Pi: "
+  HOSTNAME=$REPLY
+  if [[ $HOSTNAME == "" ]]
+  then
+    echo Please try again with a valid host name
+    exit;
+  fi
+  echo $HOSTNAME > /etc/hostname
+fi
+
+# SETUP TIMEZONE
+ln --force -s /usr/share/zoneinfo/US/Pacific /etc/localtime
+
+# SETUP SERVICES
 systemctl enable darkstat lighttpd.service
 
-chpasswd -e << EOSF
-root:\$6\$BcIn6ZXm\$dsIT5df3t.iNCQUbYMTVMuublLUUC0s4RjUknQfIPYtvpGlivPH9Srq4Ho/Oh1n/PoLuNHiH/C7O4nb6JC55A.
-EOSF
+# RESIZE THE FILESYSTEM TO MATCH PARTITION SIZE
+resize2fs /dev/mmcblk0p2
+
+reboot #25 seconds to reboot
+

c286bfcd606940bacacec55a2f33999ff7e67053
Chris Plock <plockc@gmail.com>
2013-09-15 08:07:56 -0700

    working on hostname and password switches

--- archInstall/installArchToPiFromMac.sh
+++ archInstall/installArchToPiFromMac.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: remove alarmpi keys from known hosts and ignore unknown host on ssh
-
 # stop on any errors
 set -e
 
@@ -15,26 +13,28 @@ function usage() {
 cat <<EOF
 $(basename "$0"): will install and configure arch linux on a raspberry pi
 Usage: $(basename "$0") [switches] [--]
-      -f archImage    Location of the arch linux image .iso
-      -u user         Local user that needs to be set up for ssh
-                      Defaults to the user that is running this as sudo (or root if root)
-      -h              This help
-      -p descriptor   The numerical id (like 1 for stdin) to read the password
-                      This is used by sshpass to help set up ssh connectivity to avoid terminal input
-                      Examples: echo "password" | $0 -p0 remoteUser@remoteHost
-                               $0 -p3 remoteUser@remoteHost 3<<<"password"
+      -f archImage         Location of the arch linux image .iso
+      -u user              Local user that needs to be set up for ssh
+                           Defaults to the user that is running this as sudo (or root if root)
+      -n hostname.domain   New hostname and domain for the pi
+      -h                   This help
+      -p descriptor        The numerical id (like 1 for stdin) to read the new root password
+                           This is used by sshpass to help set up ssh connectivity to avoid terminal input
+                           Examples: echo "password" | $0 -p0 remoteUser@remoteHost
+                                     read -p "new pass: " && echo $REPLY | $0 -p0 remoteUser@remoteHost
 Hint: if you see end of file without any output, make sure you can ssh without problems
 EOF
 }
 
 # setup ssh user default, can be overridden below
-if [[ -n "${SSH_USER}" ]]; then ssh_user="${SSH_USER}"; else ssh_user="${USER}"; fi
+if [[ -n "${SUDO_USER}" ]]; then ssh_user="${SUDO_USER}"; else ssh_user="${USER}"; fi
 
-# leading ':' to run silent, 'f:' means f need an argument, 'c' is just an option
-while getopts ":f:hu:p:" opt; do case $opt in
+# leading ':' to run silent, 'f:' means f need an argument, 'h' is just an option
+while getopts ":f:hu:n:p:" opt; do case $opt in
 	h)  usage; exit 0;;
-	p)  PASS="${OPTARG}"; useSshPass="-p 0";;
+	p)  NEWPASS="-p3 3<<<\"${OPTARG}\"";;
 	u)  ssh_user="${OPTARG}";;
+	n)  NEWHOSTNAME="-n ${OPTARG}";;
 	f)  if [[ ! -e "$OPTARG" ]]; then usage; echo "\$OPTARG" does not exist for -f; exit 1; fi
 	    isoFile="$OPTARG";;
 	\?) usage; echo "Invalid option: -$OPTARG" >&2; exit 1;;
@@ -51,29 +51,29 @@ bash <(curl -fsSL https://raw.github.com/plockc/DevOps/master/archInstall/imageI
 echo "=================================================================================="
 echo "Just remove the SD card (it is already ejected) and install it to the Raspberry Pi then power on the Pi, then hit enter here to continue.  When prompted, use \"root\" as the default password"
 
-read -p "Hit Enter to continue: "
+# read -p "Hit Enter to continue: "
 sleep 28
 
 # flush dns cache
 killall -HUP mDNSResponder
 
 # #############
-#  NEED TO FIGURE OUT WHICH USER TO RUN AS
+#  NEED TO TEST THIS!
 # #############
 
-su -l ${ssh_user} bash <(curl -fsSL https://raw.github.com/plockc/DevOps/master/remoteSshSetup.sh) ${useSshPass} root@alarmpi <<< "${PASS}"
+echo Setting up ssh connectivity for ${ssh_user} to root user on raspberry pi, you may be prompted for ssh key passphrases
+# the -l on the bash is needed to get the proper PATH
+su -l ${ssh_user} -c 'eval $(ssh-agent 2>/dev/null) && echo adding keys && (ssh-add || echo no keys found) && echo setting up && bash -l <(curl -fsSL https://raw.github.com/plockc/DevOps/master/remoteSshSetup.sh) -p 3 root@alarmpi 3<<< "root"'
 
 echo "=================================================================================="
-echo "Pi OS installed, now configuring and updating packages
+echo "Pi OS installed, now configuring and updating packages"
 echo "=================================================================================="
 
 # POST INSTALLATION
 set +e
 # base64 the post install file locally then the remote bash will see a file of the base64 decoded contents
-ssh -t root@alarmpi bash \<\(base64 --decode --ignore-garbage \<\<\< $(curl -fsSL https://raw.github.com/plockc/ArchDevOps/master/archInstall/archPiPostInstall.sh | base64)\) 
+# the NEWSPASSWORD must come last as it includes a file descriptor
+su -l ${ssh_user} -c 'ssh -t root@alarmpi bash \<\(base64 --decode --ignore-garbage \<\<\< $(curl -fsSL https://raw.github.com/plockc/ArchDevOps/master/archInstall/archPiPostInstall.sh | base64)\) '"${NEWHOSTNAME} ${NEWPASSWORD}"
 set -e
 
-sleep 25
-
-# really need to know the new host here, at which point we could tweak out the hostname for the key
-
+sleep 28

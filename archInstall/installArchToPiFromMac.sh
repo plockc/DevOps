e5e59ab72fcca69b5033cd03bafe9f59729b4125
Chris Plock <plockc@gmail.com>
2013-11-23 07:11:02 -0800

    reverting the fd, 60s must have been the command list as a file

--- archInstall/installArchToPiFromMac.sh
+++ archInstall/installArchToPiFromMac.sh
@@ -39,7 +39,7 @@ while getopts ":f:hu:n:p:" opt; do case $opt in
 	h)  usage; exit 0;;
 	    # user passes in the file descriptor for this script to read in the password
 	    # then we will use fd3 to pass it into the archiPiPostInstall script
-	p)  PASSFLAG="-p 3"; NEWPASSWORD=$(cat /dev/fd/6${OPTARG});;
+	p)  PASSFLAG="-p 3"; NEWPASSWORD=$(cat /dev/fd/${OPTARG});;
 	u)  ssh_user="${OPTARG}";;
 	n)  NEWHOSTNAME="${OPTARG}"; NEWHOSTNAME_POST_INSTALL_FLAG="-n ${NEWHOSTNAME}";;
 	f)  if [[ ! -e "$OPTARG" ]]; then usage; echo "\$OPTARG" does not exist for -f; exit 1; fi

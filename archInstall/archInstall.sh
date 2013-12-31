6b57a576139ba254e491a47bde2395e747058ea4
Chris Plock <plockc@gmail.com>
2013-12-31 12:14:29 -0800

    easier to read, better with squid pacman proxy, added swap, password and hostname actually work now, removed non-working dhcpcd setup

--- archInstall/archInstall.sh
+++ archInstall/archInstall.sh
@@ -6,56 +6,22 @@
 
 set -e
 
+# set the clock
+pacman --noconfirm -Syy --needed ntp && ntpd -gq && hwclock -w
+
+# confirm that we want to destroy /dev/sda completely
+echo Checking mounts . . .
 cat /proc/mounts | grep sda && (echo Error, /dev/sda is used) || echo /dev/sda does not appear to be used
-read -p "Are you sure you want to destroy /dev/sda? " -r
+read -p "Are you sure you want to completely wipe /dev/sda? " -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     echo Aborting
     exit;
 fi
 
-pacman --noconfirm -Syy ntp && ntpd -gq && hwclock -w
-
-parted --script --align optimal -- /dev/sda \
-  mklabel msdos \
-  mkpart primary ext4 63s 100M \
-  mkpart primary ext4 100MB -1G \
-  mkpart primary linux-swap -1G -1s
-
-partprobe /dev/sda
-
-mkfs.ext4 -q /dev/sda2
-mount -t ext4 /dev/sda2 /mnt
-
-mkfs.ext4 -q /dev/sda1
-mkdir -p /mnt/boot
-
-mount -t ext4 /dev/sda1 /mnt/boot
-
-mkswap /dev/sda3
-swapon /dev/sda3
-
-pacstrap /mnt base base-devel openssh augeas ntp wget darkhttpd darkstat unzip dnsutils rsync screen
-
-genfstab -p /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt <<EOF
-mkinitcpio -p linux
-pacman --noconfirm -S syslinux
-
-sed -i 's/sda3/sda2/' /boot/syslinux/syslinux.cfg
-syslinux-install_update -iam # install files(-i), set boot flag (-a), install MBR boot code (-m)
-
-ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-locale-gen # edit /etc/locate.gen possibly
-#systemctl enable dhcpcd@enp0s5
-# install network device is eth0 but runtime is emp0s5, so have to do it manually
-ln -s '/usr/lib/systemd/system/dhcpcd\@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd\@enp0s5.service'
-systemctl enable sshd.service darkstat ntpd.service dhcpcd
-
-## CHANGE ROOT PASSWORD
-
+######################################
+# GET A NEW ROOT PASSWORD
+######################################
 read -s -p "Please enter a new root password: "
 NEW_PASSWORD=$REPLY
 echo
@@ -67,20 +33,77 @@ then
   echo Passwords did not match, please try again
   exit;
 fi
-chpasswd << EOSF
-root:$NEW_PASSWORD
-EOSF
 
-## CHANGE HOSTNAME
-read -p "Please enter the full host name for this Pi: "
-HOSTNAME=$REPLY
-if [[ $HOSTNAME == "" ]]
+######################################
+## GET NEW HOSTNAME
+######################################
+read -p "Please enter the full host name for this Arch Linux instance: "
+NEW_HOSTNAME=$REPLY
+if [[ $NEW_HOSTNAME == "" ]]
 then
 echo Please try again with a valid host name
 exit;
 fi
-echo $HOSTNAME > /etc/hostname
+HOSTNAME=$NEW_HOSTNAME
 
+######################################
+# CREATE PARTITIONS AND FILESYSTEMS
+######################################
+parted --script --align optimal -- /dev/sda \
+  mklabel msdos \
+  mkpart primary ext4 63s 100M \
+  mkpart primary ext4 100MB -1G \
+  mkpart primary linux-swap -10G -1s
+partprobe /dev/sda
+
+mkfs.ext4 -q /dev/sda2
+mount -t ext4 /dev/sda2 /mnt
+
+mkfs.ext4 -q /dev/sda1
+mkdir -p /mnt/boot
+mount -t ext4 /dev/sda1 /mnt/boot
+
+# this does not seem to be working all the way
+mkswap /dev/sda3
+
+######################################
+# INSTALL PACKAGES
+######################################
+pacstrap /mnt base base-devel openssh augeas ntp wget darkhttpd darkstat unzip dnsutils rsync screen
+
+######################################
+# BASIC CONFIGURATION
+######################################
+genfstab -p /mnt >> /mnt/etc/fstab
+echo "/dev/sda3   swap   swap   defaults   0   0" >> /mnt/etc/fstab
+
+ln -s /usr/share/zoneinfo/US/Pacific /mnt/etc/localtime
+
+echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
+
+echo $NEW_HOSTNAME > /mnt/etc/hostname
+
+echo "export http_proxy='$http_proxy'" > /mnt/etc/profile.d/pacmanProxy.sh
+
+#################
+# RUN SCRIPTS
+#################
+arch-chroot /mnt <<EOF
+
+mkinitcpio -p linux
+
+pacman --noconfirm -S syslinux
+
+sed -i 's/sda3/sda2/' /boot/syslinux/syslinux.cfg
+syslinux-install_update -iam # install files(-i), set boot flag (-a), install MBR boot code (-m)
+
+locale-gen # edit /etc/locate.gen possibly
+systemctl enable sshd.service darkstat ntpd.service dhcpcd
+
+## CHANGE ROOT PASSWORD
+chpasswd << EOSF
+root:$NEW_PASSWORD
+EOSF
 
 exit # exit the chroot
 EOF
